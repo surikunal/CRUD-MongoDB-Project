902611ce63b68a372f3b69f0e25805d5e266ada5
Supreme <suri.kunal.222@gmail.com>
2022-03-03 12:32:26 +0530

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/resource/BookController.java
@@ -0,0 +1,43 @@
+package com.example.demo.resource;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Book;
+import com.example.demo.repository.BookRepository;
+
+@RestController
+public class BookController {
+	@Autowired
+	private BookRepository repository;
+	
+	@PostMapping("/saveBook")
+	public String saveBook(@RequestBody Book book) {
+		repository.save(book);
+		return "One Book saved";
+	}
+	
+	@GetMapping("/findAllBooks")
+	public List<Book> getBook() {
+		return repository.findAll();
+	}
+	
+	@GetMapping("/findAllBooks/{id}")
+	public Optional<Book> getBook(@PathVariable int id) {
+		return repository.findById(id);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public String deleteBook(@PathVariable int id) {
+		repository.deleteById(id);
+		return "Book Deleted with id :" + id;
+	}
+}
